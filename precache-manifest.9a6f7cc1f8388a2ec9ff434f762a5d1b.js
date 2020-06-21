1cd74c7cc0291f576e19fcbb5f5d0f7a62a27657
pentaline <mchambefit@gmail.com>
2020-06-21 13:05:18 -0700

    Updates

---
+++ precache-manifest.9a6f7cc1f8388a2ec9ff434f762a5d1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637bc06c42a1a72b26a11131a8b7b7e0",
+    "url": "/ChambaPortfolio/index.html"
+  },
+  {
+    "revision": "059fef3895bfdd72ac66",
+    "url": "/ChambaPortfolio/static/css/main.45aa333e.chunk.css"
+  },
+  {
+    "revision": "39a28158e323a59020b9",
+    "url": "/ChambaPortfolio/static/js/2.91a4776b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChambaPortfolio/static/js/2.91a4776b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059fef3895bfdd72ac66",
+    "url": "/ChambaPortfolio/static/js/main.ff4a2edd.chunk.js"
+  },
+  {
+    "revision": "649e6f25799a81a19af8",
+    "url": "/ChambaPortfolio/static/js/runtime-main.c8441240.js"
+  },
+  {
+    "revision": "0a57ef65ccab38d64c7444855efa2d42",
+    "url": "/ChambaPortfolio/static/media/code-background.0a57ef65.jpg"
+  },
+  {
+    "revision": "c01665a9904ee9d866cad50cace661d9",
+    "url": "/ChambaPortfolio/static/media/logo-large.c01665a9.png"
+  }
+]);
